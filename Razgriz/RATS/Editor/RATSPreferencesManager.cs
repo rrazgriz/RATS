e10979594f741cf37579d5195d05d30083b6b826
Razgriz <47901762+rrazgriz@users.noreply.github.com>
2022-09-15 23:26:54 -0400

    cleanup cleanup

--- Razgriz/RATS/Editor/RATSPreferencesManager.cs
+++ Razgriz/RATS/Editor/RATSPreferencesManager.cs
@@ -22,55 +22,108 @@ namespace Razgriz.RATS
     {
         public static string PreferencePrefix = MethodBase.GetCurrentMethod().DeclaringType.Namespace;
 
+        static void PrintPrefs(EditorPreferenceManager prefs)
+        {
+            foreach (IEditorPreference pref in prefs.GetList())
+            {
+                Debug.Log($"Pref: {pref.GetEditorPrefsID()}  Type: {pref.GetPrefType().ToString()}  Tags: {string.Join(",", pref.GetTags())}");
+            }
+        }
+
+        static void PrintFloats(EditorPreferenceManager prefs)
+        {
+            foreach (EditorPreference<float> pref in prefs.GetList<float>())
+            {
+                Debug.Log($"Pref: {pref.GetEditorPrefsID()}  Type: {pref.GetPrefType().ToString()}  Value: {pref.Value.ToString()}  Stored Value: {EditorPrefs.GetFloat(pref.GetEditorPrefsID(), -111)})");
+            }
+        }
+
         static RATSPreferencesManager()
         {
-            var Prefs = new EditorPreferenceManager(PreferencePrefix);
+            var RATSPrefs = RATSPreferences.GetInstance();
 
-            var testFloat1 = new EditorPreference<float>("aTestFloat", 1.0f, PreferencePrefix);
-            var testFloat2 = new EditorPreference<float>("aTestFloat2", 2.0f, PreferencePrefix);
-            var testInt1 = new EditorPreference<int>("aTestInt", 1, PreferencePrefix);
-            var testInt2 = new EditorPreference<int>("aTestInt2", 3, PreferencePrefix);
-            var testBool1 = new EditorPreference<bool>("aTestBool2", true, PreferencePrefix);
+            RATSPrefs.Create("aTestFloat", 1.0f, "testTag");
+            RATSPrefs.Create("aTestFloat2", 7.0f);
+            RATSPrefs.Create("aTestInt", 1);
+            RATSPrefs.Create("aTestInt2", -20);
 
-            Prefs.Add(testFloat1);
-            Prefs.Add(testInt1);
-            Prefs.Add(testFloat2);
-            Prefs.Add(testInt2);
+            RATSPrefs.Create("aTestBool", true);
+            RATSPrefs.Create("aTestString", "aString");
+            RATSPrefs.Create("aTestColor", Color.green);
 
-            Prefs.Create("aTestBool", true);
-            Prefs.Create("aTestColor", Color.green);
+            RATSPrefs.InitializeAll();
 
-            Prefs.InitializeAll();
+            PrintFloats(RATSPrefs);
+            RATSPrefs.Float("aTestFloat").Value = 2.0f;
+            RATSPrefs.Float("aTestFloat2").Value = -5.2132348957f;
+            PrintFloats(RATSPrefs);
+            RATSPrefs.SaveAll();
+            PrintFloats(RATSPrefs);
+            RATSPrefs.ResetAll("testTag");
+            PrintFloats(RATSPrefs);
+            RATSPrefs.ResetAll();
+            PrintFloats(RATSPrefs);
+            RATSPrefs.SaveAll();
+            PrintFloats(RATSPrefs);
 
-            foreach (IEditorPreference pref in Prefs.GetPrefs())
-            {
-                Debug.Log($"{pref.GetName()} : {pref.GetPrefType().ToString()}");
-            }
+        }
+    }
+
+    // Pseudo-singleton (global instance)
+    // Access as such:
+    //  var RATSPrefs = RATSPreferences.GetInstance();
+    public sealed class RATSPreferences : EditorPreferenceManager
+    {
+        private static volatile EditorPreferenceManager _instance;
+
+        private RATSPreferences() {}
+
+        // Lazy init
+        public static EditorPreferenceManager GetInstance()
+        {
+            if(_instance == null)
+                _instance  = new EditorPreferenceManager();
+            return _instance;
         }
     }
 
     public class EditorPreferenceManager
     {
         private string _prefix = MethodBase.GetCurrentMethod().DeclaringType.Namespace;
-        private string _id = "";
         private Dictionary<string, IEditorPreference> _preferences = new Dictionary<string, IEditorPreference>();
 
-        public EditorPreferenceManager(string id = "") => _id = id;
-        public EditorPreferenceManager(string prefix, string id = "")
-        {
-            _id = id;
-            _prefix = prefix;
-        }
+        // Constructor
+        public EditorPreferenceManager() {}
+        public EditorPreferenceManager(string prefix) => _prefix = prefix;
 
-        public T GetValue<T>(string name) => GetPreferenceValidated<T>(name).Value;
-        public T GetDefaultValue<T>(string name) => GetPreferenceValidated<T>(name).DefaultValue;
-        public EditorPreference<T> GetPref<T>(string name) => GetPreferenceValidated<T>(name);
-        public List<IEditorPreference> GetPrefs() => _preferences.Values.ToList();
+        // Member Handling
+        public void Create(string name, bool   defaultValue, params string[] tags) => this.Add(new EditorPreference<bool>   (name, defaultValue, _prefix, tags));
+        public void Create(string name, float  defaultValue, params string[] tags) => this.Add(new EditorPreference<float>  (name, defaultValue, _prefix, tags));
+        public void Create(string name, int    defaultValue, params string[] tags) => this.Add(new EditorPreference<int>    (name, defaultValue, _prefix, tags));
+        public void Create(string name, string defaultValue, params string[] tags) => this.Add(new EditorPreference<string> (name, defaultValue, _prefix, tags));
+        public void Create(string name, Color  defaultValue, params string[] tags) => this.Add(new EditorPreference<Color>  (name, defaultValue, _prefix, tags));
 
-        public bool Contains(string name) => _preferences.ContainsKey(name);
-        public List<string> PrefsList() => _preferences.Keys.ToList<string>();
-        public List<string> PrefsList(Type type) => _preferences.Where( pref => pref.Value.GetPrefType() == type ).Select( pref => pref.Key ).ToList();
+        public void Add(EditorPreference<bool>   pref) => _preferences.Add(pref.Name, pref);
+        public void Add(EditorPreference<int>    pref) => _preferences.Add(pref.Name, pref);
+        public void Add(EditorPreference<float>  pref) => _preferences.Add(pref.Name, pref);
+        public void Add(EditorPreference<string> pref) => _preferences.Add(pref.Name, pref);
+        public void Add(EditorPreference<Color>  pref) => _preferences.Add(pref.Name, pref);
+
+        // Access
+        public EditorPreference<bool>   Bool(string name)   => GetPreferenceValidated<bool>(name);
+        public EditorPreference<float>  Float(string name)  => GetPreferenceValidated<float>(name);
+        public EditorPreference<int>    Int(string name)    => GetPreferenceValidated<int>(name);
+        public EditorPreference<string> String(string name) => GetPreferenceValidated<string>(name);
+        public EditorPreference<Color>  Color(string name)  => GetPreferenceValidated<Color>(name);
+        public EditorPreference<T>      Get<T>(string name) => GetPreferenceValidated<T>(name);
+
+        public List<IEditorPreference> GetList() => _preferences.Values.ToList();
+        public List<EditorPreference<T>> GetList<T>() => _preferences.Where( pref => pref.Value.GetPrefType() == typeof(T) ).Select( pref => pref.Value as EditorPreference<T> ).ToList();
+
+        public List<string> PrefNames() => _preferences.Keys.ToList<string>();
+        public List<string> PrefNames<T>() => _preferences.Where( pref => pref.Value.GetPrefType() == typeof(T) ).Select( pref => pref.Key ).ToList();
         
+        // Registry Key management
         public void InitializeAll() => _preferences.Values.ToList().ForEach( pref => pref.Initialize() );
         public void SaveAll() => _preferences.Values.ToList().ForEach( pref => pref.Save() );
 
@@ -80,37 +133,21 @@ namespace Razgriz.RATS
         {
             foreach (IEditorPreference pref in _preferences.Values)
             {
-                List<string> prefTags = pref.GetTags();
-                if(prefTags.Intersect(tags).Any())
+                if(pref.GetTags().Intersect(tags).Any())
                     pref.ResetToDefault();
             }
         }
-
-        public void PreferenceAddTag(string name, string tag) => _preferences[name].AddTag(tag);
-        public void PreferenceAddTags(string name, IEnumerable<string> tags) => _preferences[name].AddTags(tags);
-
-        public void Create(string name, bool   defaultValue, IEnumerable<string> tags = null) => this.Add(new EditorPreference<bool>   (name, defaultValue, _prefix, tags));
-        public void Create(string name, float  defaultValue, IEnumerable<string> tags = null) => this.Add(new EditorPreference<float>  (name, defaultValue, _prefix, tags));
-        public void Create(string name, int    defaultValue, IEnumerable<string> tags = null) => this.Add(new EditorPreference<int>    (name, defaultValue, _prefix, tags));
-        public void Create(string name, string defaultValue, IEnumerable<string> tags = null) => this.Add(new EditorPreference<string> (name, defaultValue, _prefix, tags));
-        public void Create(string name, Color  defaultValue, IEnumerable<string> tags = null) => this.Add(new EditorPreference<Color>  (name, defaultValue, _prefix, tags));
-
-        public void Add(EditorPreference<bool>   pref) => _preferences.Add(pref.Name, pref);
-        public void Add(EditorPreference<int>    pref) => _preferences.Add(pref.Name, pref);
-        public void Add(EditorPreference<float>  pref) => _preferences.Add(pref.Name, pref);
-        public void Add(EditorPreference<string> pref) => _preferences.Add(pref.Name, pref);
-        public void Add(EditorPreference<Color>  pref) => _preferences.Add(pref.Name, pref);
 
         private EditorPreference<T> GetPreferenceValidated<T>(string name)
         {
             if (!_preferences.Keys.Contains(name))
             {
-                Debug.LogError($"[RATS] Preferences Manager: Preference {name} doesn't exist in preference group {_id}");
+                Debug.LogError($"[{_prefix}] Preferences Manager: Preference {name} doesn't exist in preferences");
                 return default(EditorPreference<T>);
             }
             else if (_preferences[name].GetPrefType() != typeof(T))
             {
-                Debug.LogError($"[RATS] Preferences Manager: Tried to access preference {name} of type {_preferences[name].GetPrefType().ToString()} with {typeof(T).ToString()}");
+                Debug.LogError($"[{_prefix}] Preferences Manager: Tried to access preference {name} of type {_preferences[name].GetPrefType().ToString()} with {typeof(T).ToString()}");
                 return default(EditorPreference<T>);
             }
             else
@@ -144,29 +181,28 @@ namespace Razgriz.RATS
 
         private readonly string _prefix = "";
         private readonly Type _type;
-        private string _name = "";
-        private List<string> _tags = new List<string>();
 
         public T Value { get; set; }
         public T DefaultValue { get; set; }
         public string Name { get; set; }
+        public List<string> Tags = new List<string>();
 
-        public EditorPreference(string name, T defaultValue, string prefix, IEnumerable<string> tags = null)
+        public EditorPreference(string name, T defaultValue, string prefix, params string[] tags)
         {
             if (!s_validTypes.Contains(typeof(T)))
-                Debug.LogError($"[RATS] Preference Manager: Tried to create EditorPreference of type {typeof(T).ToString()} - EditorPreference can only be of types: {string.Join(",", s_validTypes)}");
+                Debug.LogError($"[{_prefix}] Preferences Manager: Tried to create EditorPreference of type {typeof(T).ToString()} - EditorPreference can only be of types: {string.Join(",", s_validTypes)}");
 
             Name = name;
             DefaultValue = defaultValue;
             Value = defaultValue;
             _prefix = prefix;
             _type = typeof(T);
-            if (tags != null)
-                _tags.AddRange(tags);
+            if (tags.Length > 0)
+                Tags.AddRange(tags);
         }
 
         public string GetName() => Name;
-        public string GetEditorPrefsID() => $"{_prefix}.{_name}";
+        public string GetEditorPrefsID() => $"{_prefix}.{Name}";
 
         public Type GetPrefType() => _type;
         public List<Type> GetValidTypes() => s_validTypes;
@@ -174,9 +210,9 @@ namespace Razgriz.RATS
         public T GetValue() => Value;
         public T GetDefaultValue() => DefaultValue;
 
-        public void AddTag(string tag) => _tags.Add(tag);
-        public void AddTags(IEnumerable<string> tags) => _tags.AddRange(tags);
-        public List<string> GetTags() => _tags;
+        public void AddTag(string tag) => Tags.Add(tag);
+        public void AddTags(IEnumerable<string> tags) => Tags.AddRange(tags);
+        public List<string> GetTags() => Tags;
 
         public void ResetToDefault() => Value = DefaultValue;
 
@@ -195,7 +231,6 @@ namespace Razgriz.RATS
         public void Save()
         {
             string editorPrefsID = GetEditorPrefsID();
-
             if      (_type == typeof(bool)) EditorPrefs.SetBool(editorPrefsID, (bool)(object)Value);
             else if (_type == typeof(float)) EditorPrefs.SetFloat(editorPrefsID, (float)(object)Value);
             else if (_type == typeof(int)) EditorPrefs.SetInt(editorPrefsID, (int)(object)Value);
